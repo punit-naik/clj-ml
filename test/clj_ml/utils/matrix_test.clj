97bb4d16b11d3d29cb577a76948c7ab3d56b41f6
Punit Naik <naik.punit44@gmail.com>
2020-03-01 03:35:04 +0530

    more tests

--- test/clj_ml/utils/matrix_test.clj
+++ test/clj_ml/utils/matrix_test.clj
@@ -1,9 +1,11 @@
 (ns clj-ml.utils.matrix-test
   (:require [clojure.test :refer [deftest testing is use-fixtures]]
-            [clj-ml.utils.matrix :refer :all])
+            [clj-ml.utils.matrix :refer :all]
+            [clojure.string :as clj-str])
   (:import [clojure.lang LazySeq]))
 
 (defonce ^:private sample-identity-matrix [[1 0 0] [0 1 0] [0 0 1]])
+(defonce ^:private sample-identity-matrix-negative-values [[-1 0 0] [0 -1 0] [0 0 -1]])
 (defonce ^:private valid-2d-matrix [[1 2 3] [4 5 6]])
 (defonce ^:private valid-2d-matrix-2 [[1 2 3] [4 5 6] [7 8 9]])
 (defonce ^:private valid-2d-matrix-added-to-identity-matrix [[2 2 3] [4 6 6] [7 8 10]])
@@ -16,6 +18,7 @@
 (defonce ^:private valid-2d-matrix-each-elem-subtracted-by-2 [[-1 0 1] [2 3 4]])
 (defonce ^:private valid-2d-matrix-transposed [[1 4] [2 5] [3 6]])
 (defonce ^:private invalid-2d-matrix [[1 2 3] [4 6]])
+(defonce ^:private mat-mul-error-str "The number of columns of the first matrixare not equal to the number of rows of the second matrix")
 
 (deftest is-matrix-test
   (testing "Checking if the `clj-ml.utils.matrix/matrix?` function"
@@ -60,3 +63,18 @@
       (is (= (perform-arithmetic-op sample-identity-matrix valid-2d-matrix-2 -) valid-2d-matrix-subtracted-from-identity-matrix))
       (is (= (perform-arithmetic-op sample-identity-matrix valid-2d-matrix-2 *) valid-2d-matrix-multiplied-by-identity-matrix))
       (is (= (perform-arithmetic-op sample-identity-matrix valid-2d-matrix-2 /) valid-2d-matrix-divied-by-identity-matrix)))))
+
+(deftest matrix-multiply-test
+  (testing "If the function `clj-ml.utils.matrix/matrix-multiply` calculates the dot product of two matrices properly"
+    (is (= (matrix-multiply valid-2d-matrix-2 sample-identity-matrix) valid-2d-matrix-2))
+    (try (matrix-multiply valid-2d-matrix-2 valid-2d-matrix)
+      (catch Exception e
+        (is (= (clj-str/replace (.getMessage e) #"\n|\s\s+" "") mat-mul-error-str))))))
+
+(deftest mean-matrix-test
+  (testing "Checking if the `clj-ml.utils.matrix/mean` function calculates the mean of a 2D matrix properly"
+    (is (= (format "%.2f"(mean sample-identity-matrix)) "0.33"))))
+
+(deftest absolute-matrix-test
+  (testing "Checking if the `clj-ml.utils.matrix/absolute` function calculates the absolute of a 2D matrix properly"
+    (is (= (absolute sample-identity-matrix-negative-values) (perform-arithmetic-op sample-identity-matrix 1.0 *)))))
