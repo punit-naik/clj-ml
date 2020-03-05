6d8226f5eb42778229cd1b0c5747735302c1f326
Punit Naik <naik.punt44@gmail.com>
2020-03-05 23:14:51 +0530

    added project git url

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject org.clojars.punit-naik/clj-ml "0.1.0"
   :description "Collection of ML and it's corresponsing utilities in Clojure"
+  :url "http://github.com/punit-naik/clj-ml"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]]
