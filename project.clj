2de9f449def97fa5b4f05140e3f27d26dd723f8d
Punit Naik <naik.punt44@gmail.com>
2020-03-12 20:05:04 +0530

    removed group-id and added uberjar profile

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.punit-naik/clj-ml "0.1.0"
+(defproject clj-ml "0.1.0"
   :description "Collection of ML and it's corresponsing utilities in Clojure"
   :url "http://github.com/punit-naik/clj-ml"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
@@ -6,4 +6,5 @@
   :dependencies [[org.clojure/clojure "1.10.0"]]
   :pom-addition [:developers [:developer
                               [:name "Punit Naik"]
-                              [:email "naik.punit44@gmail.com"]]])
+                              [:email "naik.punit44@gmail.com"]]]
+  :profiles {:uberjar {:aot :all}})
