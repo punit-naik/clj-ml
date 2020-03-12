3402d6f94b0b39f78fef9f74ef1470061d534ad5
Punit Naik <naik.punt44@gmail.com>
2020-03-12 20:23:45 +0530

    updated project

--- project.clj
+++ project.clj
@@ -1,10 +1,7 @@
-(defproject org.clojars.punit-naik/clj-ml "0.1.0"
+(defproject org.clojars.punit-naik/clj-ml "0.1.2"
   :description "Collection of ML and it's corresponsing utilities in Clojure"
-  :url "http://github.com/punit-naik/clj-ml"
+  :url "https://github.com/punit-naik/clj-ml"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]]
-  :pom-addition [:developers [:developer
-                              [:name "Punit Naik"]
-                              [:email "naik.punit44@gmail.com"]]]
   :profiles {:uberjar {:aot :all}})
