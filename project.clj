e7197fb6a4d7a876750358579ff5e6423e70b978
Punit Naik <naik.punt44@gmail.com>
2020-03-12 20:19:34 +0530

    added group-id

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-ml "0.1.0"
+(defproject org.clojars.punit-naik/clj-ml "0.1.0"
   :description "Collection of ML and it's corresponsing utilities in Clojure"
   :url "http://github.com/punit-naik/clj-ml"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
