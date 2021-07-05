5ad1d52aefa995cc22fc7c0760986e41ce7104f4
Punit Naik <naik.punit44@gmail.com>
2021-07-05 16:53:07 +0530

    better levenstein distance fn

--- src/org/clojars/punit_naik/clj_ml/utils/string.clj
+++ src/org/clojars/punit_naik/clj_ml/utils/string.clj
@@ -1,30 +1,42 @@
 (ns org.clojars.punit-naik.clj-ml.utils.string)
 
-(defn levenshtein-distance
-  "Finds the percentage of match (reversed levenshtein distance & normalised)
-   between two strings"
+(defn match-char-to-str
+  "Matches a characted `c` with all characters of `string`
+   Also takes in the position of char `c` i.e. `c-pos` in it's original string
+   And an `edit-map` initial value from previous stages"
+  [edit-map-init c-pos c string]
+  (let [c-pos-zero? (zero? c-pos)]
+    (reduce (fn [[edit-distance edm]
+                 [string-char-pos string-char]]
+              (let [e (if (= c string-char) 0 1)
+                    string-char-pos-zero? (zero? string-char-pos)
+                    new-edit-distance (min
+                                       (if string-char-pos-zero?
+                                         (+ c-pos 2)
+                                         (inc edit-distance))
+                                       (if c-pos-zero?
+                                         (+ string-char-pos 2)
+                                         (inc (edm [(dec c-pos) string-char-pos])))
+                                       (+ (cond
+                                            c-pos-zero? string-char-pos
+                                            string-char-pos-zero? c-pos
+                                            :else (edm [(dec c-pos) (dec string-char-pos)]))
+                                          (if (and c-pos-zero? string-char-pos-zero?) 0 e)))]
+                [new-edit-distance (assoc edm [c-pos string-char-pos] new-edit-distance)]))
+          [(if (zero? c-pos) 0 c-pos) edit-map-init]
+          (map-indexed #(conj [] %1 %2) string))))
+
+(defn match-strings
+  "Matches two strings `s1` and `s2` and finds out their `edit distance` and `similarity`"
   [s1 s2]
-  (let [similarity (atom 0)
-        edit-map (atom {})]
-    (doseq [i (range (count s1))]
-      (let [edit (atom (if (zero? i) 0 i))]
-        (doseq [j (range (count s2))]
-          (let [e (if (= (.charAt s1 i) (.charAt s2 j)) 0 1)]
-            (reset! edit
-                    (min
-                     (if (zero? j)
-                       (+ i 2)
-                       (inc @edit))
-                     (if (zero? i)
-                       (+ j 2)
-                       (inc (@edit-map (str (dec i) "," j))))
-                     (+ (cond
-                          (zero? i) j
-                          (zero? j) i
-                          :else (@edit-map (str (dec i) "," (dec j))))
-                        (if (and (zero? i) (zero? j)) 0 e))))
-            (swap! edit-map assoc (str i "," j) @edit)
-            (when (and (= i (dec (count s1))) (= j (dec (count s2))))
-              (reset! edit-map nil)
-              (reset! similarity @edit))))))
-    (double (- 100 (* 100 (/ @similarity (+ (count s1) (count s2))))))))
+  (reduce (fn [{:keys [edit-map] :as m} [string-char-pos string-char]]
+            (let [[edit-distance edit-distance-map] (match-char-to-str edit-map string-char-pos string-char s2)]
+              (assoc m :edit-map edit-distance-map :similarity edit-distance)))
+          {:edit-map {}} (map-indexed #(conj [] %1 %2) s1)))
+
+(defn reversed-levenstein-distance
+  "Finds out the revered levenstein distance (percentage of match)
+   between two strings `s1` and `s2` by normalising the edit distance"
+  [s1 s2]
+  (let [{:keys [similarity]} (match-strings s1 s2)]
+    (double (- 100 (* 100 (/ similarity (+ (count s1) (count s2))))))))
