84d6a8a572558479f889b934ed4651b86d33be9b
punit-naik <naik.punit44@gmail.com>
2022-06-01 05:15:28 +0530

    🎨 formatting fix

--- src/org/clojars/punit_naik/clj_ml/utils/calculus.clj
+++ src/org/clojars/punit_naik/clj_ml/utils/calculus.clj
@@ -4,5 +4,7 @@
   "Finds the derivative of a function ax^n+bx^n-1+...+z represented as a
    Collection of it's coefficients [a b ... z]"
   [eq]
-  (map-indexed (fn [idx coeff]
-                 (* coeff (dec (- (count eq) idx)))) (butlast eq)))
+  (map-indexed
+   (fn [idx coeff]
+     (* coeff (dec (- (count eq) idx))))
+   (butlast eq)))
