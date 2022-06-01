03a22821dc6114fde71d7e6c37956e0f06adbf3b
punit-naik <naik.punit44@gmail.com>
2022-06-02 02:43:16 +0530

    bumped version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.punit-naik/clj-ml "1.0.4"
+(defproject org.clojars.punit-naik/clj-ml "1.0.5"
   :description "Collection of ML and it's corresponsing utilities in Clojure"
   :url "https://github.com/punit-naik/clj-ml"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
